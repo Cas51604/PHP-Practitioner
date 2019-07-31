24a81ff1a5dc0b6d3e6733d8565b98792e22ba5a
Juan Casimiro <juan.a.casimiro@outlook.com>
2019-07-31 14:44:18 -0700

    still trying

--- PHP-Practitioner/routes.php
+++ PHP-Practitioner/routes.php
@@ -3,6 +3,6 @@
 $router->define([
     'Github/PHP-Practice/PHP-Practitioner/'  => 'controllers/index.php',
     'Github/PHP-Practice/PHP-Practitioner/about' => 'controllers/about.php',
-    'Github/PHP-Practice/PHP-Practitioner/about/culture' => 'controllers/about-culture.php',
-    'Github/PHP-Practice/PHP-Practitioner/contact' => 'controllers/contact.php'
+    'about/culture' => 'controllers/about-culture.php',
+    'contact' => 'controllers/contact.php'
 ]);
